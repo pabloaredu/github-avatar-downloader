45020bdc376f5d3cb6d2cbae3b7cffcd9b545760
Pablo Aredu <pablo.adu83@gmail.com>
2018-01-23 04:37:46 +0000

    First request call

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,33 @@
 var request = require('request');
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+
+// console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+
+  var requestOptions = {
+      host: repoOwner,
+      path: repoName
+    };
+
+    request.get(requestOptions, function(response) {
+      console.log('You are getting a response')
+      // response.setEncoding('utf8');
+      // var buffer = '';
+
+      request.on('data', function(data) {
+        buffer += data;
+        console.log("on data");
+      });
+
+      // response.on('end', function() {
+      //   cb(buffer);
+      // });
+    });
+  }
+
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
