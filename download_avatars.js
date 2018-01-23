009db3cf2689424c11d34c4ac84e0f2694a1713d
Pablo Aredu <pablo.adu83@gmail.com>
2018-01-23 05:03:40 +0000

    Added function to make the request

--- download_avatars.js
+++ download_avatars.js
@@ -4,30 +4,33 @@ var request = require('request');
 // console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
+  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+  request(url, function(err, res, body) {
+    cb(err, body);
+  });
+}
 
-  var requestOptions = {
-      host: repoOwner,
-      path: repoName
-    };
 
-    request.get(requestOptions, function(response) {
-      console.log('You are getting a response')
-      // response.setEncoding('utf8');
-      // var buffer = '';
+    // request.get(requestOptions, function(response) {
+    //   // console.log('You are getting a response')
+    //   })
+    //   .on('error', function(err){
+    //     throw err;
+    //   })
+    //   .on('response', function (response) {
 
-      request.on('data', function(data) {
-        buffer += data;
-        console.log("on data");
-      });
+    //   });
 
-      // response.on('end', function() {
-      //   cb(buffer);
-      // });
-    });
-  }
 
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
+
+// function getRepoContributors(repoOwner, repoName, cb) {
+//   var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+//   request(url, function(err, res, body) {
+//     cb(err, body);
+//   });
+// }
