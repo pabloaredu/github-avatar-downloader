8add4682ce366c41bc540724b99eeefd9ba9c520
Pablo Aredu <pablo.adu83@gmail.com>
2018-02-02 04:32:34 +0000

    Checked all functionality

--- download_avatars.js
+++ download_avatars.js
@@ -1,20 +1,22 @@
 var request = require('request');
-var token = require('./secrets');
+var secrets = require('./secrets');
+var fs = require('fs');
+
+var owner = process.argv[2];
+var repo = process.argv[3];
+
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-if (repo1 === undefined  || repo2 === undefined) {
+if (owner === undefined  || repo === undefined) {
   console.log("Please, give both arguments: owner and repo");
 } else {
-
-  console.log('Welcome to the GitHub Avatar Downloader!');
-
-  function getRepoContributors(repoOwner, repoName, cb) {
+    function getRepoContributors(repoOwner, repoName, cb) {
     var options = {
       url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
       headers: {
         'User-Agent': 'pabloaredu',
-        Authorization: 'token ' + secrets.GITHUB_TOKEN
+        Authorization: secrets.GITHUB_TOKEN
       }
     };
 
@@ -22,26 +24,24 @@ if (repo1 === undefined  || repo2 === undefined) {
       cb(err, body);
     });
   }
-
-  getRepoContributors(repo1, repo2, function(err, result) {
+  getRepoContributors(owner, repo, function(err, result) {
     if (err) {
       console.error("Error:", err);
     }
     console.log(result);
     var users = JSON.parse(result);
     users.forEach(user => {
-
-      function downloadImageByURL(url, filePath) {
-        request.get(url)
-          .on('error', function (err) {
-            throw err;
-          })
-          .on('response', function (response) {
-            console.log('Response Status Code: ', response.statusCode);
-          })
-          .pipe(fs.createWriteStream(filePath));
-      }
-      downloadImageByURL(user.avatar_url, "pic/" + user.login + ".jpg");
+    function downloadImageByURL(url, filePath) {
+      request.get(url)
+        .on('error', function (err) {
+          throw err;
+        })
+        .on('response', function (response) {
+          console.log('Response Status Code: ', response.statusCode);
+        })
+        .pipe(fs.createWriteStream(filePath));
+    }
+    downloadImageByURL(user.avatar_url, "pic/" + user.login + ".jpg");
     });
-  });
+    });
 }
