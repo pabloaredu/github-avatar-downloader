140d9b9c47d6ec9f5e96e662956ee06f1903b83e
Pablo Aredu <pablo.adu83@gmail.com>
2018-02-02 03:46:47 +0000

    Added secret token

--- download_avatars.js
+++ download_avatars.js
@@ -1,11 +1,23 @@
 var request = require('request');
+var token = require('./secrets');
 
-
-// console.log('Welcome to the GitHub Avatar Downloader!');
+console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
-  request(url, function(err, res, body) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'pabloaredu',
+      Authorization: 'token ' + secrets.GITHUB_TOKEN
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
+}
+
+  request(options, function(err, res, body) {
     cb(err, body);
   });
 }
