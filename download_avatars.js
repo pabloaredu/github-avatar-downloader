2d08709e991c34a6d7bb23b671a5de43e6a4cf8e
Pablo Aredu <pablo.adu83@gmail.com>
2018-02-02 03:58:11 +0000

    Added download funcionality

--- download_avatars.js
+++ download_avatars.js
@@ -3,46 +3,45 @@ var token = require('./secrets');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-function getRepoContributors(repoOwner, repoName, cb) {
-  var options = {
-    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-    headers: {
-      'User-Agent': 'pabloaredu',
-      Authorization: 'token ' + secrets.GITHUB_TOKEN
+if (repo1 === undefined  || repo2 === undefined) {
+  console.log("Please, give both arguments: owner and repo");
+} else {
+
+  console.log('Welcome to the GitHub Avatar Downloader!');
+
+  function getRepoContributors(repoOwner, repoName, cb) {
+    var options = {
+      url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+      headers: {
+        'User-Agent': 'pabloaredu',
+        Authorization: 'token ' + secrets.GITHUB_TOKEN
+      }
+    };
+
+    request(options, function(err, res, body) {
+      cb(err, body);
+    });
+  }
+
+  getRepoContributors(repo1, repo2, function(err, result) {
+    if (err) {
+      console.error("Error:", err);
     }
-  };
+    console.log(result);
+    var users = JSON.parse(result);
+    users.forEach(user => {
 
-  request(options, function(err, res, body) {
-    cb(err, body);
+      function downloadImageByURL(url, filePath) {
+        request.get(url)
+          .on('error', function (err) {
+            throw err;
+          })
+          .on('response', function (response) {
+            console.log('Response Status Code: ', response.statusCode);
+          })
+          .pipe(fs.createWriteStream(filePath));
+      }
+      downloadImageByURL(user.avatar_url, "pic/" + user.login + ".jpg");
+    });
   });
 }
-
-  request(options, function(err, res, body) {
-    cb(err, body);
-  });
-}
-
-
-    // request.get(requestOptions, function(response) {
-    //   // console.log('You are getting a response')
-    //   })
-    //   .on('error', function(err){
-    //     throw err;
-    //   })
-    //   .on('response', function (response) {
-
-    //   });
-
-
-
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
-});
-
-// function getRepoContributors(repoOwner, repoName, cb) {
-//   var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
-//   request(url, function(err, res, body) {
-//     cb(err, body);
-//   });
-// }
